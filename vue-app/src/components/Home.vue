1896b2d9992a70e8edfb075c031e3d15e5bc9a96
Richard Tzanov <richardtzanov@gmail.com>
2018-07-23 11:41:35 +0100

    setup basic vue app

---
+++ vue-app/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{msg}}</h1>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to find a cinema'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
